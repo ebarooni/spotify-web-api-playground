aba90e92c3887980b59c87fb3fe97f6c8854d5f6
Ehsan Barooni <barooni.eh@gmail.com>
2022-08-30 13:40:02 +0200

    Added tooltip.

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -2,17 +2,20 @@ import {NgModule} from "@angular/core";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatInputModule} from "@angular/material/input";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     FontAwesomeModule,
-    MatInputModule
+    MatInputModule,
+    MatTooltipModule
   ],
   exports: [
     BrowserAnimationsModule,
     FontAwesomeModule,
-    MatInputModule
+    MatInputModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule {}
